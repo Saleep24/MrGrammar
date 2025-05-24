0c84f9ee58bca00fbbadc39e31c3e3359306e9a9
Saleep Shrestha <saleep24@gmail.com>
2025-05-24 18:06:18 -0400

    code cleaned

--- google-docs-content.js
+++ google-docs-content.js
@@ -1,7 +1,5 @@
-// Google Docs integration
 console.log("Grammar Fixer: Google Docs content script loaded");
 
-// Basic ping handler for background script communication
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "ping") {
     sendResponse({status: "ok"});
